2ea8854603134f9bed9337d628b0750714f97227
Kei <thomakmj@gmail.com>
2022-02-10 03:43:29 +0100

    done

---
+++ binary_search.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+"""
+Author: Kei Thoma
+"""
+
+def binary_search(a, key, index=0):
+    """
+    a (list) : a sorted list
+    """
+    m = len(a) // 2 # m for middle of the array
+    if a[m] == key:            return index + m
+    elif a[len(a) // 2] > key: return binary_search(a[0:m], key, index)
+    elif a[len(a) // 2] < key: return binary_search(a[m:], key, index + m)
+
+def main():
+    import random
+
+    max_n    = 10
+    summands = [random.randint(0, 5) for n in range(max_n)]
+    a        = [sum(summands[0:n]) for n in range(len(summands))]
+    index    = random.randrange(0, max_n)
+    key      = a[index]
+
+    solution = binary_search(a, key)
+    element  = a[solution]
+
+    print("List  : {}".format(a))
+    print("Index : {} | Key : {}".format(index, key))
+    print()
+    print("Solution index: {}".format(solution))
+    print("Solution index: {}".format(element))
+
+if __name__ == "__main__":
+    main()
