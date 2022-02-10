3de7a5bec7ea7640a7d20bea51fa08b2a699df1f
Kei <thomakmj@gmail.com>
2022-02-10 04:03:09 +0100

    fixed a mistake

--- unimodal.py
+++ unimodal.py
@@ -15,7 +15,7 @@ def unimodal(a):
     current_length = 1
     max_length     = 0
 
-    going_up = False # this checks if we are going up or not; we start by going down
+    going_up = None # this checks if we are going up or not; we start by going down
 
     # was not needed
     start_index = 0
@@ -25,8 +25,19 @@ def unimodal(a):
 
     for i in range(1, len(a)):
 
+        if last(i) == next(i) and going_up is None:
+            current_length += 1
+
+        elif last(i) < next(i) and going_up is None:
+            current_length += 1
+            going_up = True
+
+        elif last(i) > next(i) and going_up is None:
+            current_length += 1
+            going_up = False
+
         # in the process of going up
-        if last(i) <= next(i) and going_up == True:
+        elif last(i) <= next(i) and going_up == True:
             current_length += 1
             going_up = True
 
@@ -48,6 +59,8 @@ def unimodal(a):
             current_length += 1
             going_up = False
 
+        print("last: {} | next: {} | going_up: {} | current: {}".format(last(i), next(i), going_up, current_length))
+
     if max_length < current_length:
         max_length = current_length
     return max_length, start_index
@@ -66,19 +79,25 @@ def main():
         plt.plot([x for x in range(index, index + length)], a[index : index + length])
         plt.show()
 
+    examples1 = [
+        [1, 1, 2, 2, 2, 2, 1, 1, 1, 1],
+        [1, 2, 3, 4, 5],
+        [5, 4, 3, 2, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 2],
+        [2, 1, 1, 1, 1],
+        [1, 2, 3, 2, 1],
+        [1, 2, 3, 2, 2],
+        [1, 1, 3, 2, 2],
+    ]
+
+    for a in examples1:
+        test_example(a)
+
+
     for n in range(10):
         test_example([random.randint(1, 10) for n in range(10)])
 
-    examples = [
-        [10, 9, 8, 7],                     #4
-        [4, 5, 3, 2, 1, 3, 6, 4, 7],       #5
-        [10, 9, 8, 10, 6, 5, 4, 3, 2, 3],  #7
-        [10, 9, 8, 7, 6, 5, 4, 3, 2, 3],   #9
-        [10, 9, 8, 7, 6, 5, 4, 3, 2, 2, 3] #10
-    ]
-
-    for a in examples:
-        test_example(a)
 
 if __name__ == "__main__":
     main()
