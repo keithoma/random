242ee9814bd5816e41275df4b2238defee815909
Kei <thomakmj@gmail.com>
2022-02-14 20:42:32 +0100

    done?

--- binary_search_execution_time.py
+++ binary_search_execution_time.py
@@ -7,46 +7,39 @@ import matplotlib.pyplot as plt
 
 import binary_search
 
-class SearchAlgorithmTestSet():
-    def __init__(self, _start_n, _end_n, _repetition=100):
+class SearchAlgorithmExecutionTime():
+    def __init__(self, _start_n, _end_n, _functions, _repetition=100):
         self.start_n    = _start_n
         self.end_n      = _end_n
+        self.functions  = _functions
         self.repetition = _repetition
 
-        self.data   = self.create_data()
+        self.test_set   = self.create_test_set() # 3-dimensional matrix
+        self.best_case_list, self.average_case_list, self.worst_case_list = self.performance_data(_start_n, _end_n, _functions)
 
-    def create_test_list(self, n):
+    # TEST SET GENERATION
+    def create_test_matrix(self, n):
         a    = [random.sample(list(range(n)), n) for _ in range(self.repetition)]
         keys = [random.randrange(n) for _ in range(self.repetition)]
         return list(zip(keys, a))
     
-    def create_data(self):
-        return [self.create_test_list(n) for n in range(self.start_n, self.end_n)]
+    def create_test_set(self):
+        return [self.create_test_matrix(n) for n in range(self.start_n, self.end_n)]
 
-
-class SearchAlgorithmExecutionTime():
-    def __init__(self, _functions, _start_n, _end_n, _repetition=100):
-        self.functions  = _functions
-        self.start_n    = _start_n
-        self.end_n      = _end_n
-        self.repetition = _repetition
-
-        self.test_set   = SearchAlgorithmTestSet(_start_n, _end_n, _repetition)
-        self.best_case_list, self.average_case_list, self.worst_case_list = self.create_data(_start_n, _end_n, _functions)
-
+    # PERFORMANCE SET GENERATION
     def test_performance(self, _key, _a, _func):
         start = time.perf_counter()
         _func(_a, _key)
         end   = time.perf_counter()
         return end - start
 
-    def list_performance(self, _list_of_keys_and_a, _func):
+    def list_performance(self, matrix, _func):
         best_case  = None
         average    = []
         worst_case = None
 
-        for keys_and_a in _list_of_keys_and_a:
-            time = self.test_performance(self, keys_and_a[0], keys_and_a[1], _func)
+        for keys_and_a in matrix:
+            time = self.test_performance(keys_and_a[0], keys_and_a[1], _func)
             if best_case is None or time < best_case:
                 best_case = time
             if worst_case is None or time > worst_case:
@@ -55,29 +48,30 @@ class SearchAlgorithmExecutionTime():
 
         return best_case, sum(average) / self.repetition, worst_case
 
-    def create_data(self, _start_n, _end_n, _func):
+    def performance_data(self, _start_n, _end_n, _func):
         best_case_list = []
         average_case_list = []
         worst_case_list = []
 
-        print(self.test_set.data)
+        # print(self.test_set)
 
-        for matrix in self.test_set.data:
+        for matrix in self.test_set:
             best_case, average, worst_case = self.list_performance(matrix, _func)
             best_case_list.append(best_case)
             average_case_list.append(average)
             worst_case_list.append(worst_case)
 
-        print(best_case_list)
+        # print(best_case_list)
 
         return best_case_list, average_case_list, worst_case_list
 
     def plot(self):
         x_axis = range(self.start_n, self.end_n)
         print(self.best_case_list)
-        plt.plot(x_axis, self.best_case_list)
-        plt.plot(x_axis, self.average_case_list)
-        plt.plot(x_axis, self.worst_case_list)
+        plt.plot(x_axis, self.best_case_list, label="Best Case")
+        plt.plot(x_axis, self.average_case_list, label="Average Case")
+        plt.plot(x_axis, self.worst_case_list, label="Worst Case")
+        plt.legend()
         plt.show()
 
 def linear_search(a, key):
@@ -90,7 +84,7 @@ def main():
     # obj = SearchAlgorithmTestSet(10, 20, 3)
     # print(obj.list_of_keys_and_a(2))
 
-    obj = SearchAlgorithmExecutionTime(10, 20, 3)
+    obj = SearchAlgorithmExecutionTime(100, 1100, linear_search, 200)
     obj.plot()
     
 
--- test.py
+++
@@ -1,6 +0,0 @@
-import random
-
-print(range(10))
-print(list(range(10)))
-print(random.shuffle(list(range(10))))
-print(random.shuffle([1, 2, 3]))
